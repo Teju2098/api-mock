9d8144f1a966a97e153cc5d27c76e7d88c3a6205
tejaswi <saitejaswi.thotakura@gmail.com>
2025-07-29 22:00:42 +0200

    updated config file

--- playwright.config.js
+++ playwright.config.js
@@ -38,7 +38,7 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { 
-        headless : false,
+        //headless : false,
         ...devices['Desktop Chrome'] },
     },
 
